fe951cec908cf09ccb7f918551be9ce359589fc1
Lautaro <latirobles@gmail.com>
2025-11-10 19:30:44 -0300

    creando proyecto

---
+++ src/main/java/com/sismografo/PpaiTerceraEntregaApplication.java
@@ -0,0 +1,13 @@
+package com.sismografo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpaiTerceraEntregaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpaiTerceraEntregaApplication.class, args);
+	}
+
+}
