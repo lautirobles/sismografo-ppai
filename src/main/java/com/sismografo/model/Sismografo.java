ed6bf282f139f6a9a4a5441420d7a8fabbb1423d
Tobias <tobiasdcxn@gmail.com>
2025-11-10 20:12:15 -0300

    entidades base

---
+++ src/main/java/com/sismografo/model/Sismografo.java
@@ -0,0 +1,27 @@
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "sismografo")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Sismografo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String identificadorSismografo;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    // Relación One-to-Many con SerieTemporal
+    @OneToMany(mappedBy = "sismografo", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<SerieTemporal> seriesTemporales;
+
+    // Otros atributos según el diagrama (coordenadas, etc.)
+}
