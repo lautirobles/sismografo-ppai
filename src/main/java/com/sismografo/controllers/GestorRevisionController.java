8502454a60617c02a280dee3f6c3fbc5e4761a4c
Tobias <tobiasdcxn@gmail.com>
2025-11-11 17:22:39 -0300

    controller del gestor????

---
+++ src/main/java/com/sismografo/controllers/GestorRevisionController.java
@@ -0,0 +1,32 @@
+package com.sismografo.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sismografo.services.GestorRevisionService;
+import com.sismografo.dto.EventoSismicoDto;
+
+import java.util.List;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/gestor-revision")
+public class GestorRevisionController {
+
+    private final GestorRevisionService gestorRevisionService;
+
+    public GestorRevisionController(GestorRevisionService gestorRevisionService){
+        this.gestorRevisionService = gestorRevisionService;
+    }
+
+
+    // paso 6: el gestor busca todos los eventos no revisados, el front envia la request a /gestor-revision
+    @GetMapping
+    public List<EventoSismicoDto> buscarEventosNoRevisados(){
+        return gestorRevisionService.buscarEventosNoRevisados();
+    }
+
+
+}
