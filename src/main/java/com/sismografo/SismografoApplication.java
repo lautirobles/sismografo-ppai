8500686c833e1de7a8bc8deb871986e937427bc0
Lautaro <latirobles@gmail.com>
2025-11-11 11:51:22 -0300

    cambio de nombre a la clase principal

--- src/main/java/com/sismografo/SismografoApplication.java
+++ src/main/java/com/sismografo/SismografoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PpaiTerceraEntregaApplication {
+public class SismografoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PpaiTerceraEntregaApplication.class, args);
+		SpringApplication.run(SismografoApplication.class, args);
 	}
 
 }
